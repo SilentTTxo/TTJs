836e48c0cd4817ebb8a59794717bc313fdccdadd
unknown <tangtao@meizu.com>
2016-12-06 20:07:15 +0800

    f

--- TT.js
+++ TT.js
@@ -3,12 +3,17 @@ showMsg = function(msg){
 	$("#data")[0].innerHTML = msg;
 }
 $(function(){
+	//插入bootstrap
+	$("head").append('<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">')
+	$("head").append('<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>')
+
+	//插入提示框
 	$("body").append('<div id = "model" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">'
   + '<div class="modal-dialog modal-sm">'
   + '<div class="modal-content">'
   +   '<div class="modal-header">'
   +     '<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'
-  +     '<h4 class="modal-title" id="myModalLabel">提示</h4>'
+  +     '<h4 class="modal-title" id="myModalLabel">Issue</h4>'
   +   '</div>'
   +   '<div class="modal-body">'
   +     '<p id = data>dddd</p>'
@@ -16,4 +21,5 @@ $(function(){
   + '</div>'
   + '</div>'
   + '</div>')
+	
 })
