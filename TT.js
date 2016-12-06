da1b40f61be88bbbd610ec4fd5054628f8e76dab
unknown <tangtao@meizu.com>
2016-12-06 20:09:08 +0800

    f

--- TT.js
+++ TT.js
@@ -2,6 +2,29 @@ showMsg = function(msg){
 	$("#model").modal();
 	$("#data")[0].innerHTML = msg;
 }
+String.prototype.format = function(args) {
+    var result = this;
+    if (arguments.length > 0) {    
+        if (arguments.length == 1 && typeof (args) == "object") {
+            for (var key in args) {
+                if(args[key]!=undefined){
+                    var reg = new RegExp("({" + key + "})", "g");
+                    result = result.replace(reg, args[key]);
+                }
+            }
+        }
+        else {
+            for (var i = 0; i < arguments.length; i++) {
+                if (arguments[i] != undefined) {
+                    //var reg = new RegExp("({[" + i + "]})", "g");//这个在索引大于9时会有问题，谢谢何以笙箫的指出
+　　　　　　　　　　　　var reg= new RegExp("({)" + i + "(})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
+    }
+    return result;
+}
 $(function(){
 	//插入bootstrap
 	$("head").append('<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">')
